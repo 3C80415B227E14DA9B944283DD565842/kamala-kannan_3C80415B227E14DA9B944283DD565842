f75587b68afd44fb047484765b12f402c2b46f9a
3C80415B227E14DA9B944283DD565842 <144415085+3C80415B227E14DA9B944283DD565842@users.noreply.github.com>
2023-10-27 11:20:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# leep year using if-elif-else statement
+def isLeapYear(year):
+  if (year % 4 ==0 and year % 100!= 0) or year % 400== 0:
+    return True
+  else:
+    return False
+year = int(input("Enter a year:"))  
+if isLeapYaear(year):
+  print('{} is a leapyear.'.format(year))
+else:
+  print('{} is not a leapyear.'.format(year))
+  
